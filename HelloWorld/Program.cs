a6c6c63f675e2b75349cc4463ab7bd6fd8f3a3d2
Gabriel DM <gnizto@outlook.com>
2023-07-25 21:38:27 -0300

    Comparing loops and compression time

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,13 +6,57 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            string myString = "Hello. Every. Word. Has. A. Period.";
-            string[] myStringSplit = myString.Split(". ");
+            int[] intsToCompress = new int[] {10,15, 20, 25, 30, 12, 34};
 
-            foreach(string part in myStringSplit)
+            DateTime startTime = DateTime.Now;
+            int totalValue = 0;
+            for (int i = 0; i < intsToCompress.Length; i++)
             {
-                Console.WriteLine(part);
+                totalValue += intsToCompress[i];
             }
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+            Console.WriteLine(totalValue);
+
+            startTime = DateTime.Now;
+            totalValue = 0;
+            foreach(int intForCompression in intsToCompress)
+            {
+                totalValue += intForCompression;
+            }
+
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+            Console.WriteLine(totalValue);
+            
+            startTime = DateTime.Now;
+            totalValue = 0;
+            int index = 0;
+            while(index < intsToCompress.Length)
+            {
+                totalValue += intsToCompress[index];
+                index++;
+            }
+            
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+            Console.WriteLine(totalValue);
+            
+            startTime = DateTime.Now;
+            totalValue = 0;
+            index = 0;
+    
+            do{
+                totalValue += intsToCompress[index];
+                index++;
+            }
+            while(index < intsToCompress.Length);
+            
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+            Console.WriteLine(totalValue);
+
+            startTime = DateTime.Now;
+            // totalValue = 0;
+            totalValue = intsToCompress.Sum();
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+            Console.WriteLine(totalValue);
         }
     }
 }
