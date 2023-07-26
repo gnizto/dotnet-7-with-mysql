92f45f07a70e92d8a63167e3518aa792e800e036
Gabriel DM <gnizto@outlook.com>
2023-07-25 22:44:29 -0300

    Added method

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,56 +7,31 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             int[] intsToCompress = new int[] {10,15, 20, 25, 30, 12, 34};
+            int totalValue;
 
             DateTime startTime = DateTime.Now;
+
+            totalValue = intsToCompress.Sum();
+
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+            Console.WriteLine(totalValue);
+
+            startTime = DateTime.Now;
+
+            totalValue = GetSum(intsToCompress:intsToCompress);
+
+            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
+            Console.WriteLine(totalValue);
+        }
+        static private int GetSum(int[] intsToCompress)
+        {
             int totalValue = 0;
             for (int i = 0; i < intsToCompress.Length; i++)
             {
                 totalValue += intsToCompress[i];
             }
-            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
-            Console.WriteLine(totalValue);
 
-            startTime = DateTime.Now;
-            totalValue = 0;
-            foreach(int intForCompression in intsToCompress)
-            {
-                totalValue += intForCompression;
-            }
-
-            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
-            Console.WriteLine(totalValue);
-            
-            startTime = DateTime.Now;
-            totalValue = 0;
-            int index = 0;
-            while(index < intsToCompress.Length)
-            {
-                totalValue += intsToCompress[index];
-                index++;
-            }
-            
-            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
-            Console.WriteLine(totalValue);
-            
-            startTime = DateTime.Now;
-            totalValue = 0;
-            index = 0;
-    
-            do{
-                totalValue += intsToCompress[index];
-                index++;
-            }
-            while(index < intsToCompress.Length);
-            
-            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
-            Console.WriteLine(totalValue);
-
-            startTime = DateTime.Now;
-            // totalValue = 0;
-            totalValue = intsToCompress.Sum();
-            Console.WriteLine((DateTime.Now - startTime).TotalSeconds);
-            Console.WriteLine(totalValue);
+            return totalValue;
         }
     }
 }
